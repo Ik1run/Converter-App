47a90c6f3e07e7fe321f2ebb22da78406d8d53dc
Davy <davixsmit@gmail.com>
2024-09-16 15:03:06 +0200

    Adds Converter App

---
+++ MAUIVERTER/MAUIVERTER/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using MAUIVERTER.MVVM.Views;
+
+namespace MAUIVERTER
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new NavigationPage(new MenuView());
+        }
+    }
+}
